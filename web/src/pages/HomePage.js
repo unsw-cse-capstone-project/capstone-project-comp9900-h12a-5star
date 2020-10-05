56b62e7dcee25d806479e9885dfafe5856ccd24a
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-10-06 02:47:06 +1100

    Added grid for all the sections

--- web/src/pages/HomePage.js
+++ web/src/pages/HomePage.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Container, Header, Divider } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import _ from 'lodash'
+import { Container, Header, Divider, Grid } from 'semantic-ui-react';
 import NavBar from '../components/NavBar';
 import MovieTile from '../components/MovieTile';
 
-const HomePage = () => (
-    <React.Fragment>
-        <NavBar />
-        <Container style={{ margin: 20 }}>
-            <Header as='h1'>Top Movies</Header>
-            <MovieTile />
-            <Divider section />
-            <Header as='h1'>Top Rated</Header>
-            <MovieTile />
-            <Divider section />
-            <Header as='h1'>Most Popular</Header>
-            <MovieTile />
-        </Container>
-    </React.Fragment>
-);
+const columnsTopMovies = _.times(5, (i) => (
+    <Grid.Column key={i}>
+      <MovieTile />
+    </Grid.Column>
+))
+const columnsRecentlyReleased = _.times(5, (i) => (
+    <Grid.Column key={i}>
+      <MovieTile />
+    </Grid.Column>
+))
+const columnsMostPopular = _.times(5, (i) => (
+    <Grid.Column key={i}>
+      <MovieTile />
+    </Grid.Column>
+))
 
-export default HomePage;
+export default class HomePage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />
+               
+                <Container style={{ margin: 20 }}>
+                    <Header as='h1'>Top Rated Movies</Header>
+                    <Grid columns='equal'>{columnsTopMovies}</Grid>
+                    <Divider section />
+                    <Header as='h1'>Recently Released Movies </Header>
+                    <Grid columns='equal'>{columnsRecentlyReleased}</Grid>
+                    <Divider section />
+                    <Header as='h1'>Popular Movies</Header>
+                    <Grid columns='equal'>{columnsMostPopular}</Grid>
+                </Container>
+            </React.Fragment>
+        )
+    }
+}

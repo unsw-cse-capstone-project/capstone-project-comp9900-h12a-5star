2764720f8de3aec914aff328f7c28ce0759e932c
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-10-06 03:33:45 +1100

    added see more option for all categories

--- web/src/pages/HomePage.js
+++ web/src/pages/HomePage.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import _ from 'lodash'
-import { Container, Header, Divider, Grid } from 'semantic-ui-react';
+import { Container, Header, Divider, Grid, Label } from 'semantic-ui-react';
 import NavBar from '../components/NavBar';
 import MovieTile from '../components/MovieTile';
 
 const columnsTopMovies = _.times(5, (i) => (
     <Grid.Column key={i}>
-      <MovieTile />
+        <MovieTile />
     </Grid.Column>
 ))
 const columnsRecentlyReleased = _.times(5, (i) => (
     <Grid.Column key={i}>
-      <MovieTile />
+        <MovieTile />
     </Grid.Column>
 ))
 const columnsMostPopular = _.times(5, (i) => (
     <Grid.Column key={i}>
-      <MovieTile />
+        <MovieTile />
     </Grid.Column>
 ))
 
@@ -25,15 +25,42 @@ export default class HomePage extends Component {
         return (
             <React.Fragment>
                 <NavBar />
-               
+
                 <Container style={{ margin: 20 }}>
-                    <Header as='h1'>Top Rated Movies</Header>
+                    <Grid columns="equal">
+                        <Grid.Column>
+                            <Header as='h1'>Top Rated Movies</Header>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Label as='a' color='blue' ribbon='right' onClick={event => window.location.href = '/searchResult/topRated'}>
+                                see more
+                            </Label>
+                        </Grid.Column>
+                    </Grid>
                     <Grid columns='equal'>{columnsTopMovies}</Grid>
                     <Divider section />
-                    <Header as='h1'>Recently Released Movies </Header>
+                    <Grid columns="equal">
+                        <Grid.Column>
+                            <Header as='h1'>Recently Released Movies</Header>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Label as='a' color='blue' ribbon='right' onClick={event => window.location.href = '/searchResult/recentReleased'}>
+                                see more
+                            </Label>
+                        </Grid.Column>
+                    </Grid>
                     <Grid columns='equal'>{columnsRecentlyReleased}</Grid>
                     <Divider section />
-                    <Header as='h1'>Popular Movies</Header>
+                    <Grid columns="equal">
+                        <Grid.Column>
+                            <Header as='h1'>Popular Movies</Header>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Label as='a' color='blue' ribbon='right' onClick={event => window.location.href = '/searchResult/popularMovies'}>
+                                see more
+                            </Label>
+                        </Grid.Column>
+                    </Grid>
                     <Grid columns='equal'>{columnsMostPopular}</Grid>
                 </Container>
             </React.Fragment>

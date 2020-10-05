e5b426d13b98bd2455272c01e5ce162a4a6c00d1
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-10-06 01:50:44 +1100

    Added movie tile with all the required fields

--- web/src/pages/HomePage.js
+++ web/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from '../components/NavBar';
+import MovieTile from '../components/MovieTile';
 
 const HomePage = () => (
     <React.Fragment>
         <NavBar />
         <Container style={{ margin: 20 }}>
             <p>The content will appear here</p>
+            <MovieTile />
         </Container>
     </React.Fragment>
 );

5820aee19ed659c4df06b523f201f88765721280
korupalli <45527890+korupalli@users.noreply.github.com>
2020-10-08 18:37:58 +1100

    Added multiselection forms

--- api/lg/forms.py
+++ api/lg/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms.models import ModelForm
+from django_select2.forms import Select2MultipleWidget
 
 from .models import UserProfile
 
@@ -10,7 +11,10 @@ User._meta.get_field('email')._unique = True
 class ProfileForm(ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('age', 'desc','Language')
+        fields = ('Gender','Languages','Genre')
+        widgets = {
+            'interests': Select2MultipleWidget,
+        }
 
 
 class UserForm(ModelForm):
@@ -18,7 +22,7 @@ class UserForm(ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('username', 'password', 'email','first_name','last_name')
 
     def save(self, commit=True):
         new_user = User.objects.create_user(self.cleaned_data['username'],

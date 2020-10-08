e39c3eb193a4964fe604f2476b1291cbd31b1e25
korupalli <45527890+korupalli@users.noreply.github.com>
2020-10-08 18:37:20 +1100

    Added multiselection fields

--- api/lg/models.py
+++ api/lg/models.py
@@ -1,9 +1,28 @@
 from django.contrib.auth.models import User
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User , on_delete=models.CASCADE)
-    age = models.IntegerField(default=0)
-    desc = models.CharField(max_length=140,blank=True)
-    Language = models.CharField(max_length=140,blank=True)
+    Genre_CHOICES = (
+        ('Horror', 'Horror'),
+        ('Fiction', 'Fiction'),
+        ('Comedy', 'Comedy'),
+        ('Action', 'Action'),
+     )
+    Genre = MultiSelectField(choices = Genre_CHOICES, default= None)
+
+    Language_CHOICES = (
+        ('English', 'English'),
+        ('Spanish', 'Spanish'),
+        ('Chinkish', 'Chinkish'),
+        ('Hindhi', 'Hindhi'),
+     )
+    Languages = MultiSelectField(choices = Language_CHOICES, default= None)
+
+    Gender_choices = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    Gender = models.CharField(max_length=6,choices=Gender_choices,default=None)
+

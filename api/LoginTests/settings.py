65fc667492fe83148e28d35114914f0d066727d3
korupalli <45527890+korupalli@users.noreply.github.com>
2020-10-08 18:36:08 +1100

    Update settings.py

--- api/LoginTests/settings.py
+++ api/LoginTests/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lg.apps.LgConfig',
-    'rest_framework'
+    'rest_framework',
+    'multiselectfield'
 ]
 
 MIDDLEWARE = [

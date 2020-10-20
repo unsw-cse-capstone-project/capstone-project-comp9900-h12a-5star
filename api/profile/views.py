0dc9819de84afb5a342bf4eb62c719edd0b9ef4b
Rohith Korupalli <rohithkorupalli@gmail.com>
2020-10-20 13:44:39 +1100

    Added API for fetching profile data

--- api/profile/views.py
+++ api/profile/views.py
@@ -5,26 +5,32 @@ from rest_framework.permissions import IsAuthenticated
 #from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from profile.models import UserProfile
 
+STATUS_CODE = status.HTTP_400_BAD_REQUEST
+RESPONSE = {
+    'success': 'false',
+    'status code': status.HTTP_400_BAD_REQUEST,
+    'message': 'User does not exists',
+    }
 
 class UserProfileView(RetrieveAPIView):
-
     def get(self, request, *args, **kwargs):
         try:
-            user_profile = UserProfile.objects.get(user=request.user)
+            user_profile = UserProfile.objects.get(username=request.data['username'])
             status_code = status.HTTP_200_OK
             response = {
                 'success': 'true',
                 'status code': status_code,
                 'message': 'User profile fetched successfully',
                 'data': {
-                    'first_name': user_profile.firstname,
-                    'last_name': user_profile.lastname,
+                    'firstname': user_profile.firstname,
+                    'lastname': user_profile.lastname,
                     'gender': user_profile.gender,
-                    }
-                }
-
+                    'genres':user_profile.genres,
+                    'languages':user_profile.languages}}
         except Exception as e:
-            status_code = status.HTTP_400_BAD_REQUEST
+            RESPONSE['error']= str(e)
+            return Response(RESPONSE, status=STATUS_CODE)
+        return Response(response, status=status_code)
             response = {
                 'success': 'false',
                 'status code': status.HTTP_400_BAD_REQUEST,

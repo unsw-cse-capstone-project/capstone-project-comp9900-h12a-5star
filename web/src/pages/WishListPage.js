792f19691eac28c0f80b4e503ae1b6333a67ae98
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-10-21 12:50:03 +1100

    updated username fields

--- web/src/pages/WishListPage.js
+++ web/src/pages/WishListPage.js
@@ -26,7 +26,7 @@ export default class WishListPage extends Component {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: "shubmat", reviewerUsername: "shubmat" })
+            body: JSON.stringify({ username: this.user, reviewerUsername: this.props.match.params.userId })
         };
 
         fetch("http://127.0.0.1:8000/api/viewWishlist/", requestOptions)

dc430fad565d4a55f7e2cc1afb719f53f6f14da3
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-10-26 15:52:36 +1100

    updated the  interface

--- web/src/pages/ProfilePage.js
+++ web/src/pages/ProfilePage.js
@@ -1,5 +1,5 @@
-import React , { Component } from 'react'
-import { Tab, Container, Grid, Card, Icon, Image, Button, Input, Header, Divider } from 'semantic-ui-react'
+import React , { Component, createRef } from 'react'
+import { Tab, Container, Grid, Card, Icon, Image, Button, Input, Header, Divider,Sticky, Rail, Ref } from 'semantic-ui-react'
 import NavBar from '../components/NavBar';
 
 
@@ -33,14 +33,17 @@ export default class WishListPage extends Component {
         this.setState({editDisabled: true})
     }
 
+    contextRef = createRef()
+
     render() {
 
         return(
             <React.Fragment>
                 
                 <NavBar />
-                
+                <Ref innerRef={this.contextRef}>
                 <Container>
+                
                     <div style={{backgroundImage: `url(${require("../images/profileBackground.jpg")})`, height: 300}}>
                         <br />
                         <br />
@@ -52,25 +55,31 @@ export default class WishListPage extends Component {
                             
                             <Header as= "h1">Mathew Wade</Header>
                             <Input transparent={this.state.editTransparent} disabled={this.state.editDisabled} placeholder='Mathew'  />
-                                {
-                                    (this.state.edit) &&
-                                    <div>
-                                        <br/>
-                                        <Button size={"small"} circular color={"red"} onClick={this.handleClick_cancelEdit}> 
-                                            <Icon name='close' /> 
-                                            Cancel 
-                                        </Button>
-                                    </div>
-                                }
+                            <br />
+                            <Input transparent={this.state.editTransparent} disabled={this.state.editDisabled} placeholder='Mathew'  />
+                            <br />
+                            <Input transparent={this.state.editTransparent} disabled={this.state.editDisabled} placeholder='Mathew'  />
+                                
                                 <br />
                                 <br />
                                 <Divider />
+                            {
+                                (!this.state.edit)?
                             <Button color={"purple"} onClick={this.handleClick_edit}>
                                 <center>
                                     <br />
                                     <Icon name='pencil' size="big" /> <br /> <br /> Edit Profile
                                 </center>
                             </Button>
+                            
+                                 :
+                                <Button color={"red"} onClick={this.handleClick_cancelEdit}>
+                                    <center>
+                                        <br />
+                                        <Icon name='pencil square' size="big" /> <br /> <br /> Cancel Edit
+                                    </center>
+                                </Button>
+                            }
                             <Button color={"purple"} onClick={this.handleClick_edit}>
                                 <center>
                                     <br />
@@ -83,10 +92,58 @@ export default class WishListPage extends Component {
                                     <Icon name='eye' size="big" /> <br /> <br /> Watch list
                                 </center>
                             </Button>
-                            
+                            <br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br /><br />
+                                <br />
                         </center>
                     </div>
+                    
+                                <Sticky context={this.contextRef} >
+                                    <Button color={"purple"} onClick={this.handleClick_edit}>
+                                        <center>
+                                            <br />
+                                            <Icon name='remove user' size="big" /> <br /> <br /> Baned User
+                                        </center>
+                                    </Button>
+                                    <Button color={"purple"} onClick={this.handleClick_edit}>
+                                        <center>
+                                            <br />
+                                            <Icon name='eye' size="big" /> <br /> <br /> Watch list
+                                        </center>
+                                    </Button>
+                                </Sticky>
+                            
+                    
                 </Container>
+                </Ref>
+                
             </React.Fragment>
         );
     }

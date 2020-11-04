72e458ab1133f214af88e0396d9ac0d2ed8c3be6
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-11-04 23:32:38 +1100

    null point errors

--- web/src/components/MovieTile.js
+++ web/src/components/MovieTile.js
@@ -31,47 +31,47 @@ import {
       return (
         <React.Fragment>
             <Card className='movie_card'>
-      <Dimmer.Dimmable
-          as={Image}
-          dimmed={active}
-          dimmer={{ active, content }}
-          onMouseEnter={this.handleShow}
-          onMouseLeave={this.handleHide}
-          size='medium'
-          src= {this.props.poster}
-          className='movie_image'
-        />
-      <Card.Content>
-        <Card.Header>{this.props.title.substring(0,24)}</Card.Header>
-        <Card.Meta><Rating icon='star' defaultRating={1} maxRating={1} disabled/> {this.props.rating}  </Card.Meta>
-        <Card.Description>
-          {
-            (this.props.description)?
-            <p>
-              {this.props.description.substring(0,100)} ... 
-            </p>
-            :
-            <p>
-              Description not available
-            </p>
-          }
-          
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        {
-          (this.props.release)?
-          <p>
-            Released in {this.props.release.substring(0,4)}
-          </p>
-          :
-          <p>
-            Release date not available
-          </p>
-        }
-         
-      </Card.Content>
-    </Card> 
+            <Dimmer.Dimmable
+                as={Image}
+                dimmed={active}
+                dimmer={{ active, content }}
+                onMouseEnter={this.handleShow}
+                onMouseLeave={this.handleHide}
+                size='medium'
+                src= {this.props.poster}
+                className='movie_image'
+              />
+            <Card.Content>
+              <Card.Header>{this.props.title.substring(0,24)}</Card.Header>
+              <Card.Meta><Rating icon='star' defaultRating={1} maxRating={1} disabled/> {this.props.rating}  </Card.Meta>
+              <Card.Description>
+                {
+                  (this.props.description)?
+                  <p>
+                    {this.props.description.substring(0,100)} ... 
+                  </p>
+                  :
+                  <p>
+                    Description not available
+                  </p>
+                }
+                
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              {
+                (this.props.release)?
+                <p>
+                  Released in {this.props.release.substring(0,4)}
+                </p>
+                :
+                <p>
+                  Release date not available
+                </p>
+              }
+              
+            </Card.Content>
+          </Card> 
         </React.Fragment>
         
       )

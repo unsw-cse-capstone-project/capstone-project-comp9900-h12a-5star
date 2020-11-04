eef3af552067fb6909f1367c3e529a556bb5ff3b
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-11-04 23:28:15 +1100

    null point errors

--- web/src/components/MovieTile.js
+++ web/src/components/MovieTile.js
@@ -11,25 +11,20 @@ import {
 
     
     state ={}
-    // state = this.setState(this.props.value)
-    // mut  = this.props.mutateState()
-    // 
   
     handleShow = () => this.setState({ active: true })
     handleHide = () => this.setState({ active: false })
-  
-    clickViewDetails = () => {
-        // window.location.href={`/movieDetails/${this.props.MovieTile}`}
-    }
 
     render() {
       const { active } = this.state
       
       const content = (
         <div>
-          <Button primary><Link style={{ color: '#FFF' }} className="MovieDetails" key={this.props.movieId} to= {`/movieDetails/${this.props.movieId}`}>
-            View Details
-        </Link></Button>
+          <Button primary>
+            <Link style={{ color: '#FFF' }} className="MovieDetails" key={this.props.movieId} to= {`/movieDetails/${this.props.movieId}`}>
+              View Details
+          </Link>
+        </Button>
         </div>
       )
   
@@ -50,11 +45,31 @@ import {
         <Card.Header>{this.props.title.substring(0,24)}</Card.Header>
         <Card.Meta><Rating icon='star' defaultRating={1} maxRating={1} disabled/> {this.props.rating}  </Card.Meta>
         <Card.Description>
-          {this.props.description.substring(0,100)} ... 
+          {
+            (this.props.description)?
+            <p>
+              {this.props.description.substring(0,100)} ... 
+            </p>
+            :
+            <p>
+              Description not available
+            </p>
+          }
+          
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        Released in {this.props.release.substring(0,4)} 
+        {
+          (this.props.release)?
+          <p>
+            Released in {this.props.release.substring(0,4)}
+          </p>
+          :
+          <p>
+            Release date not available
+          </p>
+        }
+         
       </Card.Content>
     </Card> 
         </React.Fragment>

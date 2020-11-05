3c54aa976a79ae85e59ea91a8e963de47ccfbbb5
Rohith Korupalli <rohithkorupalli@gmail.com>
2020-11-06 01:57:09 +1100

    no message

--- api/user/views.py
+++ api/user/views.py
@@ -1,6 +1,4 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from user.serializers import UserRegistrationSerializer
 from user.serializers import UserLoginSerializer

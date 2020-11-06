967e91d20b648806539bef8d608632cd137daafa
Rohith Korupalli <rohithkorupalli@gmail.com>
2020-11-06 23:50:46 +1100

    API to recommend a movie to other user

--- api/notifications/views.py
+++ api/notifications/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from notifications.models import notifications
 
-# Create your views here.
+
+@api_view(['POST', ])
+def suggestMovie(request):
+    new = notifications()
+    new.toUsername = request.data['toUser']
+    new.fromUsername = request.data['fromUser']
+    new.movieId = request.data['movieId']
+    new.type = 'SUGGESTION'
+    new.save()
+
+    response = {
+            'success': 'true',
+    }
+    return Response(response, status=status.HTTP_200_OK)

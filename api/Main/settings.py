d8c90b16cc6b9e819c6d5c492ce9d51d0dfb2187
Nagendra Reddy <56853807+Nagendra1995@users.noreply.github.com>
2020-11-07 01:28:49 +1100

    Update settings.py
    Added Cache Database

--- api/Main/settings.py
+++ api/Main/settings.py
@@ -108,6 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 

f5a50ae389dc874556f05083d0e4fde252065336
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-11-14 05:25:27 +1100

    redesigned banned user list for consistancy

--- web/src/pages/BannedUsers.js
+++ web/src/pages/BannedUsers.js
@@ -103,59 +103,56 @@ export default class WishListPage extends Component {
                     {/* <Header as='h1'>{this.props.match.params.userId}'s Wishlist</Header> */}
                     <Divider horizontal></Divider>
                     <Divider horizontal>
-                    <Header as='h1'>
-                        {this.props.match.params.userId.charAt(0).toUpperCase() + this.props.match.params.userId.slice(1)}'s List of banned users
+                        <Header as='h1'>
+                            {this.props.match.params.userId.charAt(0).toUpperCase() + this.props.match.params.userId.slice(1)}'s List of banned users
                     </Header>
                     </Divider>
                     {
-                    (this.state.items.data && this.state.items.data.length !== 0)?
-                    <Grid columns='equal' divided={'vertically'}>
-                        {
-                            this.state.items.data.map((item)=>
-                            <Grid.Row>
-                                <Grid.Column width={2}>
-                                    <Image src={item.profilePic} size='tiny'  />
-                                </Grid.Column>
-                                <Grid.Column width={2}>
-                                <br/>
-                                
-                        <p>{item.firstname.toUpperCase()}</p>
-                        <p>{item.lastname.toUpperCase()}</p>
+                        (this.state.items.data && this.state.items.data.length !== 0) ?
+                            <Grid columns='equal' divided={'vertically'}>
+                                {
+                                    this.state.items.data.map((item) =>
+                                        <Grid.Row>
+                                            <Grid.Column width={2}>
+                                                <Image src={item.profilePic} size='tiny' />
+                                            </Grid.Column>
+                                            <Grid.Column width={2}>
+                                                <br />
 
-                                    
-                                </Grid.Column>
-                                <Grid.Column width={8}>
-                                <br/><br/>
-                                
-                     
-                               
-                                    
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <br/><br/>
-                                    {
-                                        (this.props.match.params.userId === window.sessionStorage.getItem('username'))?
-                                            <Button circular floated='center' color='red' icon='close' onClick={()=>this.removeFromWishlist(item)} />
-                                            :
-                                            <Button primary floated='right'><Link style={{ color: '#FFF'}} className="MovieDetails" key={item.movieID} to= {`/movieDetails/${item.movieID}`}>
-                                            View Details
-                                        </Link><Icon name='right chevron' />  </Button> 
-                                    }
-                                    
-                            </Grid.Column>
-                        </Grid.Row>
-                            )
-                        }
-                        
-                    </Grid>
-                    :
-                    <Message>
-                        <Message.Header>Your Banned List is Empty</Message.Header>
-                        <p>
-                            
-                        </p>
-                    </Message>
-                }
+                                                <p>{item.firstname.charAt(0).toUpperCase() + item.firstname.slice(1)}</p>
+                                                <p>{item.lastname.charAt(0).toUpperCase() + item.lastname.slice(1)}</p>
+                                            </Grid.Column>
+                                            <Grid.Column width={8}>
+                                                <p></p>
+
+                                                <Header as="h1">{item.username.charAt(0).toUpperCase() + item.username.slice(1)}</Header>
+
+                                            </Grid.Column>
+                                            <Grid.Column>
+                                                <br /><br />
+                                                {
+                                                    (this.props.match.params.userId === window.sessionStorage.getItem('username')) ?
+                                                        <Button circular floated='center' color='red' icon='close' onClick={() => this.removeFromWishlist(item)} />
+                                                        :
+                                                        <Button primary floated='right'><Link style={{ color: '#FFF' }} className="MovieDetails" key={item.movieID} to={`/movieDetails/${item.movieID}`}>
+                                                            View Details
+                                        </Link><Icon name='right chevron' />  </Button>
+                                                }
+
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                    )
+                                }
+
+                            </Grid>
+                            :
+                            <Message>
+                                <Message.Header>Your Banned List is Empty</Message.Header>
+                                <p>
+
+                                </p>
+                            </Message>
+                    }
                 </Container>
                 
                 
